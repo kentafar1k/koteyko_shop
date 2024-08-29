aa4fba49fabb4297b1bfa05f462ec17f6b6f0c68
kentafar1k <cherald1337@yandex.ru>
2024-08-29 20:51:11 +0300

    Initial commit

--- products/views.py
+++ products/views.py
@@ -2,11 +2,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
 from products.models import ProductCategory, Product, Basket
 
-
-# Create your views here.
 
 def index(request):
     context = {
